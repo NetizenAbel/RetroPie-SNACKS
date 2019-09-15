23d2abb486d989aefe0434c695ce7c7d17d53c6a
Kashaiahyah85 <kashaiahyah85@icloud.com>
2019-09-15 15:21:06 -0700

    modified:   Project II - Final Fantasy IV.sh 	new file:   Project II - Final Fantasy IV.xml 	renamed:    Project II - Final Fantasy IV.png -> wheel/Project II - Final Fantasy IV.png

--- Project II - Final Fantasy IV.sh
+++ Project II - Final Fantasy IV.sh
@@ -1,1 +1,24 @@
-ln -s /pi/RetroPie/roms/snes/Final\ Fantasy\ II\ \(USA\).zip /pi/RetroPie/roms/snesh/Project\ II\ -\ Final\ Fantasy\ IV.zip
+#!/usr/bin/env bash
+
+HACKS_DIR="/pi/RetroPie/roms/snesh"
+ROMS_DIR="/pi/RetroPie/roms/snes"
+WHEELS_DIR="${HACKS_DIR}/wheel"
+
+#
+# Project II - Final Fantasy IV
+#
+HACK="Project\ II\ -\ Final\ Fantasy\ IV"
+ROM="Final\ Fantasy\ II \(USA\).zip"
+WHEEL="./wheel/${HACK}.png"
+XML="$(cat ${HACK}.xml)"
+
+
+function installHack (){
+    cp "${HACK}.ips" $HACKS_DIR
+    cp "$WHEEL" "$WHEELS_DIR"
+    ln -s "${ROMS_DIR}/${ROM}" "${HACKS_DIR}/${HACK}.zip"
+    cp $HACKS_DIR/gamelist.xml $HACKS_DIR/gamelist.xml.bak
+    sed "\'2i\${XML}\\\'" $HACKS_DIR/gamelist.1.xml
+}
+
+installHack
