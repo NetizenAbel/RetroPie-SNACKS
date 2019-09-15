d0784cd24e31b459d2218b5eebf38bdd49a4701b
Kashaiahyah85 <kashaiahyah85@icloud.com>
2019-09-15 16:02:35 -0700

    fixup

--- Project II - Final Fantasy IV.sh
+++ Project II - Final Fantasy IV.sh
@@ -5,11 +5,11 @@ readonly HACKS_DIR="/home/pi/RetroPie/roms/snesh"
 readonly ROMS_DIR="/home/pi/RetroPie/roms/snes"
 
 function installHack () {
-    cp $PATCH.ips $HACKS_DIR
-    cp $WHEEL $HACKS_DIR/wheel/
-    ln -s $ROM $HACKS_DIR/$PATCH.zip
+    cp "$PATCH".ips "$HACKS_DIR"
+    cp "$WHEEL" "$HACKS_DIR/wheel/"
+    ln -s "$ROMS_DIR/$ROM" "$HACKS_DIR/$PATCH.zip"
     cp $HACKS_DIR/gamelist.xml $HACKS_DIR/gamelist.xml.bak
-    sed -i '$ d' $HACKS_DIR/gamelist.xml
+    sed -i '$d' $HACKS_DIR/gamelist.xml
     cat $PATCH.xml >> $HACKS_DIR/gamelist.xml
 }
 
